df90f45735eed1add1e1ed2dfa61c1a56f4e3411
c41x <jakubduracz@gmail.com>
2014-12-10 09:20:03 +0100

    + skewer config file

--- init.el
+++ init.el
@@ -248,6 +248,7 @@
 (add-hook 'js2-mode-hook 'skewer-mode)
 (add-hook 'css-mode-hook 'skewer-css-mode)
 (add-hook 'html-mode-hook 'skewer-html-mode)
+(load "~/.emacs.d/skewer-config")
 ;(require 'simple-httpd)
 ;(setq httpd-root "d:/repo/minigame2")
 
