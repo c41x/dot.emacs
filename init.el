d34ed92cb49f6916198d09e2e7c0ec2c66695141
c41x <jakubduracz@gmail.com>
2015-03-09 12:14:34 +0100

    + comment / uncomment under C-; c

--- init.el
+++ init.el
@@ -122,6 +122,9 @@
 (global-set-key (kbd "C-; .") (lambda () (interactive) (move-frame-with-buffer 'right)))
 (global-set-key (kbd "C-; ,") (lambda () (interactive) (move-frame-with-buffer 'left)))
 
+;; comment/uncomment
+(global-set-key (kbd "C-; c") 'comment-or-uncomment-region)
+
 (cua-mode 1) ; cua-mode (Ctrl+C,V,X,Z)
 (setq x-select-enable-clipboard t) ; allows to copy/paste text between emacs and other apps
 
