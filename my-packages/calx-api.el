2806072e72d038b96bf75c0c07e8a564190ba9ce
c41x <jakubduracz@gmail.com>
2015-04-12 14:23:59 +0200

    + api rewrite

--- my-packages/calx-api.el
+++ my-packages/calx-api.el
@@ -5,6 +5,16 @@
 
 (require 'url)
 
+;; shold define (defvar calx--server-api-url "http://xyz.com/")
+(defvar calx--server-api-url "unknown")
+(defvar calx--server-api-username "unknown")
+(defvar calx--server-api-password "unknown")
+(defvar calx--keymap (make-sparse-keymap))
+
+(when calx--keymap
+  (define-key calx--keymap (kbd "C-x C-s") 'calx-set)
+  (define-key calx--keymap (kbd "C-x k") 'calx-logout))
+
 (defun calx--request (url &optional data callback-ok callback-fail)
   (let ((url-request-method "POST")
 	(url-request-extra-headers
@@ -20,98 +30,56 @@
 			  (cond ((or (string= "forbidden" errm))
 				 (message errm)
 				 (kill-buffer (current-buffer))
-				 (funcall callback-fail errm))
+				 (and callback-fail (funcall callback-fail errm)))
 				((or (string= "success" errm)
 				     (string= "logged out" errm)
 				     (string= "ok" errm))
 				 (message errm)
 				 (kill-buffer (current-buffer))
-				 (funcall callback-ok errm))
+				 (and callback-ok (funcall callback-ok errm)))
 				(t
 				 (delete-region (point-min) (point))
 				 (rename-buffer "* TODO *")
 				 (org-mode)
 				 (buffer-enable-undo)
-				 (funcall callback-ok errm))))))))
+				 (use-local-map (make-composed-keymap calx--keymap org-mode-map))
+				 (and callback-ok (funcall callback-ok errm)))))))))
 
-(defun calx--request-login ()
+(defun calx--request-login (callback)
   (calx--request (concat calx--server-api-url "/api_login")
 		 (format "username=%s&password=%s"
 			 calx--server-api-username
 			 calx--server-api-password)
-		 (lambda (res)
-		   (calx--request (concat calx--server-api-url "/api_get")))))
-
-(calx--request-login)
-
-
-
-
-(defun calx--request-callback-new-buffer (status)
-  (switch-to-buffer (current-buffer))
-  (rename-buffer "*todo*")
-  (org-mode)
-  (goto-char (point-min))
-  (search-forward "\n\n")
-  (delete-region (point-min) (point))
-  (buffer-enable-undo (current-buffer)))
-
-(defun calx--request-callback-status (status)
-  (switch-to-buffer (current-buffer))
-  (goto-char (point-min))
-  (search-forward "\n\n")
-  (message (buffer-substring-no-properties (point) (point-max)))
-  (kill-buffer (current-buffer)))
-
-(defun calx--http-post (url callback &optional data)
-  (let ((url-request-method "POST")
-    (url-request-extra-headers
-     '(("Content-Type" . "application/x-www-form-urlencoded")))
-    (url-request-data data))
-    (url-retrieve url callback)))
-
-;; shold define (defvar calx--server-api-url "http://xyz.com/")
-(defvar calx--server-api-url "unknown")
-(defvar calx--server-api-username "unknown")
-(defvar calx--server-api-password "unknown")
-
-(defun calx--login (username password)
-  (calx--http-post (concat calx--server-api-url "/api_login")
-		   (lambda (status)
-		     (calx--request-callback-status status)
-		     (calx-get))
-		   (format "username=%s&password=%s" username password)))
-
-(defun calx ()
-  (interactive)
-  (calx--login calx--server-api-username calx--server-api-password))
-
-(defun calx-login (username password)
-  (interactive "sLogin: \nsPassword:")
-  (calx--login username password))
+		 callback))
 
 (defun calx-get ()
   (interactive)
-  (calx--http-post (concat calx--server-api-url "/api_get")
-		   'calx--request-callback-new-buffer))
+  (switch-to-buffer (get-buffer-create "* TODO *"))
+  (kill-buffer)
+  (calx--request (concat calx--server-api-url "/api_get")
+		 nil
+		 nil
+		 (lambda (res) (calx--request-login (lambda (res) (calx-get))))))
 
 (defun calx-set ()
   (interactive)
-  (calx--http-post (concat calx--server-api-url "/api_set")
-		   'calx--request-callback-status
-		   (concat "text=" (url-hexify-string (buffer-string)))))
+  (calx--request (concat calx--server-api-url "/api_set")
+		 (concat "text=" (url-hexify-string (buffer-string)))
+		 nil
+		 (lambda (res) (calx--request-login (lambda (res) (calx-set))))))
+
+(defun calx ()
+  (interactive)
+  (calx--request-login
+   (lambda (res)
+     (calx--request (concat calx--server-api-url "/api_get")))))
 
 (defun calx-logout ()
   (interactive)
-  (calx--http-post (concat calx--server-api-url "/api_logout")
-		   (lambda (status)
-		     (calx--request-callback-status status)
-		     (kill-buffer (current-buffer)))))
-
-;; TODO: custom keybind
-(add-hook 'org-mode-hook (lambda ()
-			   (local-set-key (kbd "C-x C-s") 'calx-set)
-			   (local-set-key (kbd "C-x k") 'calx-logout)))
+  (calx--request (concat calx--server-api-url "/api_logout")
+		 nil
+		 (lambda (status)
+		   (kill-buffer (current-buffer)))))
 
 (provide 'calx-api)
 ;;; calx-api.el ends here
