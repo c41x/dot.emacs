f3d7b7bb468537ca7df529cf116d3b0a81c59227
c41x <jakubduracz@gmail.com>
2015-08-25 12:00:35 +0200

    + box api removed

--- init.el
+++ init.el
@@ -295,67 +295,7 @@
 ;; no pasting for MMB
 (global-unset-key (kbd "<mouse-2>"))
 
-;; TODO: property list
-;; ;; box api (in progress)
-;; (defun mbox:hide-overlay (ov)
-;;   (when (overlay-get ov 'is-box)
-;;     (overlay-put ov 'invisible nil)
-;;     (overlay-put ov 'prv-before-string (overlay-get ov 'before-string))
-;;     (overlay-put ov 'before-string "")))
-
-;; (defun mbox:show-overlay (ov)
-;;   (when (and (overlay-get ov 'is-box)
-;; 	     (overlay-get ov 'prv-before-string))
-;;     (overlay-put ov 'invisible t)
-;;     (overlay-put ov 'before-string (overlay-get ov 'prv-before-string))
-;;     (overlay-put ov 'prv-before-string nil)))
-
-;; (defun mbox:overlays-in-this-line ()
-;;   (overlays-in (line-beginning-position) (line-end-position)))
-
-;; (defun mbox:line (line column width frame-type text)
-;;   (save-excursion
-;;     (goto-line line)
-;;     ;;(mapc 'mbox:delete-if-box (mbox:overlays-in-this-line))
-;;     (let ((column-offset (+ (line-beginning-position) column))
-;; 	  (max-width (min (- (window-width) column) width))
-;; 	  (ov nil)
-;; 	  (left (point))
-;; 	  (right (point))
-;; 	  (boxes 0))
-;;       (mapc 'mbox:hide-overlay (mbox:overlays-in-this-line))
-;;       ;; find left position
-;;       (while (and (<= (save-excursion (forward-char 1) (current-column)) column)
-;; 		  (<= (+ (point) 1) (line-end-position)))
-;; 	(forward-char 1))
-;;       (setq boxes (current-column))
-;;       (setq left (min (point) (min column-offset (line-end-position))))
-;;       ;; find right position
-;;       (while (and (<= (save-excursion (forward-char 1) (current-column)) (min (window-width) (+ column width)))
-;; 		  (<= (+ (point) 1) (line-end-position)))
-;; 	(forward-char 1))
-;;       (setq right (min (point) (line-end-position)))
-;;       ;; create overlay
-;;       (setq ov (make-overlay left right))
-;;       (overlay-put ov 'is-box t)
-;;       (overlay-put ov 'invisible t)
-;;       (overlay-put ov 'before-string
-;; 		   (concat (make-string (- column boxes) ?\ )
-;; 			   (cond ((equal frame-type 'top)
-;; 				  (concat (make-string 1 ?\╔)
-;; 					  (make-string (- max-width 2) ?\═)
-;; 					  (make-string 1 ?\╗)))
-;; 				 ((equal frame-type 'bottom)
-;; 				  (concat (make-string 1 ?\╚)
-;; 					  (make-string (- max-width 2) ?\═)
-;; 					  (make-string 1 ?\╝)))
-;; 				 (t (concat (make-string 1 ?\║)
-;; 					    (substring text 0 (min (- max-width 2) (length text)))
-;; 					    (make-string (max 0 (- max-width 2 (length text))) ?\ )
-;; 					    (make-string 1 ?\║))))))
-;;       (mapc 'mbox:show-overlay (mbox:overlays-in-this-line))
-;;       ;; return overlay
-;;       ov)))
+;; TODO: property list ?
 
 ;;//- plugins
 ;; automatic brackets {}()[]""'' pairing
