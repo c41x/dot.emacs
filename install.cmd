dc242928765599d712295ef9923cadcf51d9d22c
c41x <jakubduracz@gmail.com>
2015-09-30 13:22:03 +0200

    + added boxy.el

--- install.cmd
+++ install.cmd
@@ -5,6 +5,8 @@ git clone https://github.com/c41x/ceh.el.git
 move ceh.el .emacs.d/ceh
 git clone https://github.com/c41x/hnr.el.git
 move hnr.el .emacs.d/hnr
+git clone https://github.com/c41x/boxy.el.git
+move boxy.el .emacs.d/boxy
 set currentpath=%~dp0
 setx HOME %currentpath%
 pause
