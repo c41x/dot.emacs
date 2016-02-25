3e4421c22e71974ae2d8bf8ac68b3ffe02ba174e
c41x <jakubduracz@gmail.com>
2016-02-25 11:37:37 +0100

    + install script for linux

---
+++ install.sh
@@ -0,0 +1,9 @@
+cd ~
+git clone https://github.com/c41x/dot.emacs.git
+mv dot.emacs .emacs.d
+git clone https://github.com/c41x/ceh.el.git
+mv ceh.el .emacs.d/ceh
+git clone https://github.com/c41x/hnr.el.git
+mv hnr.el .emacs.d/hnr
+git clone https://github.com/c41x/boxy.el.git
+mv boxy.el .emacs.d/boxy
