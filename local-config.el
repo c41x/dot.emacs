c16fa8ebb184cd97b6451692e902f11e5bd0e015
c41x <jakubduracz@gmail.com>
2016-04-05 17:51:42 +0200

    + sample local-config.el

---
+++ local-config.el
@@ -0,0 +1,8 @@
+(setq calx--server-api-url "http://host.com")
+(setq calx--server-api-username "username")
+(setq calx--server-api-password "password")
+(setq calx--server-backup t) ;; do perform backup?
+(setq calx--server-backup-path "/home/user/tmp/TODO-backup") ;; path to backup directory
+
+;(require 'simple-httpd)
+;(setq httpd-root "d:/repo/minigame2")
