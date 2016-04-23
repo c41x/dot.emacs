313a014aabab16453a5a08e6afad28198d9ee160
c41x <jakubduracz@gmail.com>
2016-04-23 20:12:04 +0200

    + option to clone as SSH

--- install.sh
+++ install.sh
@@ -1,10 +1,28 @@
-cd ~
-git clone https://github.com/c41x/dot.emacs.git
-mv dot.emacs .emacs.d
-git clone https://github.com/c41x/ceh.el.git
-mv ceh.el .emacs.d/ceh
-git clone https://github.com/c41x/hnr.el.git
-mv hnr.el .emacs.d/hnr
-git clone https://github.com/c41x/boxy.el.git
-mv boxy.el .emacs.d/boxy
+#!/bin/bash
+
+read -n1 -p "Set remote to SSH? [y,n]" doit
+case $doit in
+    y|Y) cd ~
+	 git clone git@github.com:c41x/dot.emacs.git
+	 mv dot.emacs .emacs.d
+	 git clone git@github.com:c41x/ceh.el.git
+	 mv ceh.el .emacs.d/ceh
+	 git clone git@github.com:c41x/hnr.el.git
+	 mv hnr.el .emacs.d/hnr
+	 git clone git@github.com:c41x/boxy.el.git
+	 mv boxy.el .emacs.d/boxy
+	 ;;
+    n|N) cd ~
+	git clone https://github.com/c41x/dot.emacs.git
+	mv dot.emacs .emacs.d
+	git clone https://github.com/c41x/ceh.el.git
+	mv ceh.el .emacs.d/ceh
+	git clone https://github.com/c41x/hnr.el.git
+	mv hnr.el .emacs.d/hnr
+	git clone https://github.com/c41x/boxy.el.git
+	mv boxy.el .emacs.d/boxy
+	;;
+    *) echo aborting ;;
+esac
+
 rm -f install.sh
