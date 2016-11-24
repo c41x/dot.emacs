1f87970944d80fd3eea65473b717088327467288
c41x <jakubduracz@gmail.com>
2016-11-24 11:31:57 +0100

    + helm git grep: no not search in submodules

--- init.el
+++ init.el
@@ -406,6 +406,7 @@
 (set 'helm-input-idle-delay 0.0)
 (when (eq system-type 'windows-nt)
   (defun helm-git-submodule-grep-process ()))
+(setq helm-git-grep-sources '(helm-git-grep-sources)) ;; only search in git, not in submodules
 
 ;; force helm to use bottom of the screen and to not break window layout
 (add-to-list 'display-buffer-alist
