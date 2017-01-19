173f7a3521e6f76c834428ec509f3c7050221440
c41x <jakubduracz@gmail.com>
2017-01-19 13:05:57 +0100

    + linux install script

--- install.sh
+++ install.sh
@@ -3,26 +3,21 @@
 read -n1 -p "Set remote to SSH? [y,n]" doit
 case $doit in
     y|Y) cd ~
-         git clone git@github.com:c41x/dot.emacs.git
-         git clone git@github.com:c41x/ceh.el.git
-         git clone git@github.com:c41x/hnr.el.git
-         git clone git@github.com:c41x/boxy.el.git
-         git clone git@github.com:c41x/recall.el.git
+         git clone git@github.com:c41x/dot.emacs.git .emacs.d
+         cd .emacs.d
+         git clone git@github.com:c41x/ceh.el.git ceh
+         git clone git@github.com:c41x/hnr.el.git hnr
+         git clone git@github.com:c41x/boxy.el.git boxy
+         git clone git@github.com:c41x/recall.el.git recall
          ;;
     n|N) cd ~
-        git clone https://github.com/c41x/dot.emacs.git
-        git clone https://github.com/c41x/ceh.el.git
-        git clone https://github.com/c41x/hnr.el.git
-        git clone https://github.com/c41x/boxy.el.git
-        git clone https://github.com/c41x/recall.el.git
+        git clone https://github.com/c41x/dot.emacs.git .emacs.d
+        cd .emacs.d
+        git clone https://github.com/c41x/ceh.el.git ceh
+        git clone https://github.com/c41x/hnr.el.git hnr
+        git clone https://github.com/c41x/boxy.el.git boxy
+        git clone https://github.com/c41x/recall.el.git recall
         ;;
     *) echo aborting ;;
 esac
 
-mv dot.emacs/* .emacs.d
-mv ceh.el .emacs.d/ceh
-mv hnr.el .emacs.d/hnr
-mv boxy.el .emacs.d/boxy
-mv recall.el .emacs.d/recall
-
-rm -f install.sh
